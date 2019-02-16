21f5317f54cacf8e53669de2e2fbd907ecb3ebf3
Ricci Rich <riccirich@Riccis-MacBook-Pro.local>
2019-02-16 13:02:41 +0200

    Add user state interface

---
+++ src/app/user/state/user.reducer.ts
@@ -0,0 +1,15 @@
+export interface UserState {
+  isUserMasked: boolean;
+}
+
+export function reducer(state, action) {
+  switch (action.type) {
+    case 'MASK_USER_NAME':
+      return {
+        ...state,
+        isUserMasked: action.payload,
+      };
+    default:
+      return state;
+  }
+}
