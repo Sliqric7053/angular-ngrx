b8669041da4ebc79d55c8ca21601c70380c01bd8
Ricci Rich <riccirich@Riccis-MacBook-Pro.local>
2019-02-17 10:02:23 +0200

    Strongly type actions in the reducer and add more cases

--- src/app/products/state/product.reducer.ts
+++ src/app/products/state/product.reducer.ts
@@ -1,6 +1,7 @@
 import { Product } from '../product';
 import * as fromRoot from '../../app-state/app.state';
 import { createFeatureSelector, createSelector } from '@ngrx/store';
+import { ProductActions, ProductActionTypes } from './product.actions';
 
 export interface AppState extends fromRoot.AppState {
   products: ProductState;
@@ -35,12 +36,39 @@ export const getProducts = createSelector(
   state => state.products
 );
 
-export function reducer(state = initialState, action): ProductState {
+export function reducer(
+  state = initialState,
+  action: ProductActions
+): ProductState {
   switch (action.type) {
-    case 'TOGGLE_PRODUCT_CODE':
+    case ProductActionTypes.ToggleProductCode:
       return {
         ...state,
         showProductCode: action.payload,
+      };
+
+    case ProductActionTypes.SetCurrentProduct:
+      return {
+        ...state,
+        currentProduct: { ...action.payload },
+      };
+
+    case ProductActionTypes.ClearCurrentProduct:
+      return {
+        ...state,
+        currentProduct: null,
+      };
+
+    case ProductActionTypes.InitializeCurrentProduct:
+      return {
+        ...state,
+        currentProduct: {
+          id: 0,
+          productName: '',
+          productCode: 'New',
+          description: '',
+          starRating: 0,
+        },
       };
 
     default:
