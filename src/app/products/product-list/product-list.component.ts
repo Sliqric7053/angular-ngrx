de24bb9576171a08a43d373f4ac963b50e4a9f7b
Ricci Rich <riccirich@Riccis-MacBook-Pro.local>
2019-02-17 10:03:03 +0200

    Refactor product list to dispatch actions using new interface

--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -7,6 +7,7 @@ import { ProductService } from '../product.service';
 
 import { Store, select } from '@ngrx/store';
 import * as fromProduct from '../state/product.reducer';
+import * as productActions from '../state/product.actions';
 
 @Component({
   selector: 'pm-product-list',
@@ -55,11 +56,7 @@ export class ProductListComponent implements OnInit, OnDestroy {
   }
 
   checkChanged(value: boolean): void {
-    this.displayCode = value;
-    this.store.dispatch({
-      payload: value,
-      type: 'TOGGLE_PRODUCT_CODE',
-    });
+    this.store.dispatch(new productActions.ToggleProductCode(value));
   }
 
   newProduct(): void {
