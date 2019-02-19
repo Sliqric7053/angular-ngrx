f38ebe53900d754ec74dfb6f7cf1ec6714a337b6
Ricci Rich <riccirich@Riccis-MacBook-Pro.local>
2019-02-19 13:48:07 +0200

    Refactor product reducer and add updateProduct cases

--- src/app/products/state/product.reducer.ts
+++ src/app/products/state/product.reducer.ts
@@ -9,14 +9,14 @@ export interface AppState extends fromRoot.AppState {
 
 export interface ProductState {
   showProductCode: boolean;
-  currentProduct: Product;
+  currentProductId: number;
   products: Product[];
   error: string;
 }
 
 const initialState: ProductState = {
   showProductCode: true,
-  currentProduct: null,
+  currentProductId: 0 || null,
   products: [],
   error: '',
 };
@@ -28,9 +28,29 @@ export const getShowProductCode = createSelector(
   state => state.showProductCode
 );
 
+export const getCurrentProductId = createSelector(
+  getProductFeatureState,
+  state => state.currentProductId
+);
+
 export const getCurrentProduct = createSelector(
   getProductFeatureState,
-  state => state.currentProduct
+  getCurrentProductId,
+  (state, currentProductId) => {
+    if (currentProductId === 0) {
+      return {
+        id: 0,
+        productName: '',
+        productCode: 'New',
+        description: '',
+        starRating: 0,
+      };
+    } else {
+      return currentProductId
+        ? state.products.find(product => product.id === currentProductId)
+        : null;
+    }
+  }
 );
 
 export const getProducts = createSelector(
@@ -57,25 +77,19 @@ export function reducer(
     case ProductActionTypes.SetCurrentProduct:
       return {
         ...state,
-        currentProduct: { ...action.payload },
+        currentProductId: action.payload.id,
       };
 
     case ProductActionTypes.ClearCurrentProduct:
       return {
         ...state,
-        currentProduct: null,
+        currentProductId: null,
       };
 
     case ProductActionTypes.InitializeCurrentProduct:
       return {
         ...state,
-        currentProduct: {
-          id: 0,
-          productName: '',
-          productCode: 'New',
-          description: '',
-          starRating: 0,
-        },
+        currentProductId: 0,
       };
     case ProductActionTypes.LoadSuccess:
       return {
@@ -89,7 +103,21 @@ export function reducer(
         products: [],
         error: action.payload,
       };
-
+    case ProductActionTypes.UpdateSuccess:
+      const updatedProducts = state.products.map(product =>
+        product.id === action.payload.id ? action.payload : product
+      );
+      return {
+        ...state,
+        currentProductId: action.payload.id,
+        products: updatedProducts,
+        error: '',
+      };
+    case ProductActionTypes.UpdateFail:
+      return {
+        ...state,
+        error: action.payload,
+      };
     default:
       return state;
   }
